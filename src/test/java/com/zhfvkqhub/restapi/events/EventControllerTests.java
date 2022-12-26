b2296289f97e4e8c7c8309e5b4012aa35445eade
kimdayeon <hamoey@naver.com>
2022-12-27 00:14:35 +0900

    test : Add test description annotation

--- src/test/java/com/zhfvkqhub/restapi/events/EventControllerTests.java
+++ src/test/java/com/zhfvkqhub/restapi/events/EventControllerTests.java
@@ -1,6 +1,7 @@
 package com.zhfvkqhub.restapi.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zhfvkqhub.restapi.common.TestDescription;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -30,6 +31,7 @@ public class EventControllerTests {
     ObjectMapper objectMapper;
 
     @Test
+    @TestDescription("정상적으로 이벤트를 생성하는 테스트")
     public void createEvent() throws Exception{
         EventDTO event = EventDTO.builder()
                 .name("Spring")
@@ -60,6 +62,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력 받을 수 없는 값을 사용한 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request() throws Exception{
         Event event = Event.builder()
                 .id(100)
@@ -88,6 +91,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력 값이 비어있는 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request_Empty_Input() throws Exception {
         EventDTO eventDTO = EventDTO.builder().build();
 
@@ -99,6 +103,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력 값이 잘못된 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request_Wrong_Input() throws Exception {
         EventDTO eventDTO = EventDTO.builder()
                 .name("Spring")
